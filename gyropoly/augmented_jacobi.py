Bug fix for normal derivative, get inertial waves running

gyropoly/augmented_jacobi.py
@@ -86,8 +86,10 @@ class AugmentedJacobiSystem():
     def rho(self, z):
         return self.__polynomial_product.evaluate(z, weighted=False)
 
-    def rhoprime(self, z):
-        return self.__polynomial_product.derivative(z)
+    def rhoprime(self, z, weighted=True):
+        if not weighted and self.num_augmented_factors != 1:
+            raise ValueError('Unweighted rhoprime multiplication only supported for one augmented factor')
+        return self.__polynomial_product.derivative(z, weighted=weighted)
 
     def augmented_weight(self, z):
         return self.__polynomial_product.evaluate(z, weighted=True)
@@ -151,8 +153,11 @@ class PolynomialProduct():
         c = self.powers if weighted else np.ones(n, int)
         return np.prod([factor(z)**c for factor, c in zip(self.factors, c)], axis=0)
 
-    def derivative(self, z):
-        n, c = len(self), self.powers
+    def derivative(self, z, weighted=True):
+        n = len(self)
+        if not weighted and n != 1:
+            raise ValueError('Unweighted derivative evaluation only supported for single-factor polynomial products')
+        c = self.powers if weighted else np.ones(n)
         f, fprime = [[g(z) for g in feval] for feval in [self.__factors, self.__derivatives]]
         products = [np.prod([f[j] for j in range(n) if j != i], axis=0) for i in range(n)]
         return np.sum([c[i]*fprime[i]*products[i] for i in range(n)], axis=0)
@@ -642,7 +647,7 @@ def embedding_operator(kind, system, n, dtype='float64', internal='float128', **
     return diags(bands, offsets, shape=(n,n), dtype=dtype)
 
 
-def rhoprime_multiplication(system, n, dtype='float64', internal='float128', **recurrence_kwargs):
+def rhoprime_multiplication(system, n, weighted=True, dtype='float64', internal='float128', **recurrence_kwargs):
     parity = system.has_even_parity
     use_jacobi_quadrature = recurrence_kwargs.pop('use_jacobi_quadrature', False)
 
@@ -652,7 +657,7 @@ def rhoprime_multiplication(system, n, dtype='float64', internal='float128', **r
         offsets = offsets[::2]
 
     def fun(z):
-        return system.rhoprime(z) * system.polynomials(n, z, dtype=internal, **recurrence_kwargs)
+        return system.rhoprime(z, weighted=weighted) * system.polynomials(n, z, dtype=internal, **recurrence_kwargs)
 
     bands = project(system, n, m, fun, offsets, dtype=internal, use_jacobi_quadrature=use_jacobi_quadrature, **recurrence_kwargs)
     return diags(bands, offsets, shape=(n+m,n), dtype=dtype)
@@ -905,7 +910,8 @@ def operator(name, factors, dtype='float64', internal='float128', **recurrence_k
     if len(factors) == 1 and name == 'C':
         name = ('C', 0)
     if name == 'rhoprime':
-        return AugmentedJacobiOperator.rhoprime(factors, dtype=dtype, internal=internal, **recurrence_kwargs)
+        weighted = recurrence_kwargs.pop('weighted', True)
+        return AugmentedJacobiOperator.rhoprime(factors, weighted=weighted, dtype=dtype, internal=internal, **recurrence_kwargs)
     return AugmentedJacobiOperator(name, factors, dtype=dtype, internal=internal, **recurrence_kwargs)
 
 
@@ -931,8 +937,9 @@ def operators(factors, dtype='float64', internal='float128', **recurrence_kwargs
     AugmentedJacobiOperator
 
     """
-    def dispatch(name):
-        return operator(name, factors, dtype=dtype, internal=internal, **recurrence_kwargs)
+    def dispatch(name, **kwargs):
+        kwargs = {**kwargs, **recurrence_kwargs}
+        return operator(name, factors, dtype=dtype, internal=internal, **kwargs)
     return dispatch
 
 
@@ -1104,11 +1111,11 @@ class AugmentedJacobiOperator():
         return Operator(factors,Z,AugmentedJacobiCodomain(1,0,0,(0,)*nc))
 
     @staticmethod
-    def rhoprime(factors, dtype='float64', internal='float128', **recurrence_kwargs):
+    def rhoprime(factors, weighted=True, dtype='float64', internal='float128', **recurrence_kwargs):
         @decorators.cached
         def R(n,a,b,c):
             system = AugmentedJacobiSystem(a, b, zip(factors,c))
-            op = rhoprime_multiplication(system, n, dtype=dtype, internal=internal, **recurrence_kwargs)
+            op = rhoprime_multiplication(system, n, weighted=weighted, dtype=dtype, internal=internal, **recurrence_kwargs)
             return infinite_csr(op)
         nc = len(factors)
         dn = PolynomialProduct(factors).total_degree(weighted=False)-1

gyropoly/stretched_cylinder.py
@@ -12,55 +12,125 @@ __all__ = ['Basis', 'total_num_coeffs', 'coeff_sizes', 'operators'
 
 class Basis():
     def __init__(self, cylinder_type, h, m, Lmax, Nmax, alpha, sigma, eta=None, t=None, has_m_scaling=True, has_h_scaling=True, dtype='float64'):
-        _check_radial_degree(Lmax, Nmax)
-        if cylinder_type not in ['full', 'half']:
-            raise ValueError(f"Invalid cylinder_type (={cylinder_type}).  Must be either 'full' or 'half'")
         _check_cylinder_type(cylinder_type)
-        self.cylinder_type = cylinder_type
-        self.h, self.m, self.Lmax, self.Nmax = h, m, Lmax, Nmax
-        self.alpha, self.sigma = alpha, sigma
-        self.dtype = dtype
+        _check_radial_degree(Lmax, Nmax)
+        self.__cylinder_type = cylinder_type
+        self.__h, self.__m, self.__Lmax, self.__Nmax = h, m, Lmax, Nmax
+        self.__alpha, self.__sigma = alpha, sigma
+        self.__dtype = dtype
 
         # Get the number of coefficients including truncation
-        self.num_coeffs = total_num_coeffs(Lmax, Nmax)
+        self.__num_coeffs = total_num_coeffs(Lmax, Nmax)
 
         # Construct the radial polynomial systems
-        self.systems = [ajacobi.AugmentedJacobiSystem(alpha, m+sigma, [(h,2*ell+2*alpha+1)]) for ell in range(Lmax)]
+        self.__systems = [ajacobi.AugmentedJacobiSystem(alpha, m+sigma, [(h,2*ell+2*alpha+1)]) for ell in range(Lmax)]
 
         # Construct the polynomials if eta and t are not None
-        self.has_m_scaling, self.has_h_scaling = has_m_scaling, has_h_scaling
-        self.P, self.Q = None, None
+        self.__has_m_scaling, self.__has_h_scaling = has_m_scaling, has_h_scaling
+        self.__P, self.__Q, self.__t, self.__eta = (None,)*4
         self._make_vertical_polynomials(eta)
         self._make_radial_polynomials(t)
 
     @property
+    def cylinder_type(self):
+        return self.__cylinder_type
+
+    @property
+    def h(self):
+        return self.__h
+
+    @property
+    def m(self):
+        return self.__m
+
+    @property
+    def Lmax(self):
+        return self.__Lmax
+
+    @property
+    def Nmax(self):
+        return self.__Nmax
+
+    @property
+    def alpha(self):
+        return self.__alpha
+
+    @property
+    def sigma(self):
+        return self.__sigma
+
+    @property
+    def dtype(self):
+        return self.__dtype
+
+    @property
+    def num_coeffs(self):
+        return self.__num_coeffs
+
+    @property
+    def has_m_scaling(self):
+        return self.__has_m_scaling
+
+    @property
+    def has_h_scaling(self):
+        return self.__has_h_scaling
+
+    @property
     def vertical_polynomials(self):
-        return self.P
+        return self.__P
 
     @property
     def radial_polynomials(self):
-        return self.Q
+        return self.__Q
 
-    def vertical_polynomial(self, ell, eta=None):
+    @property
+    def t(self):
+        return self.__t
+
+    @property
+    def eta(self):
+        return self.__eta
+
+    @decorators.cached
+    def s(self):
+        t = self.t
+        if t is None:
+            raise ValueError('Basis constructed without valid t coordinate')
+        return np.sqrt((1+t)/2)
+
+    @decorators.cached
+    def z(self):
+        t, eta = self.t, self.eta
+        if t is None:
+            raise ValueError('Basis constructed without valid t coordinate')
+        if eta is None:
+            raise ValueError('Basis constructed without valid eta coordinate')
+        tt, ee = t[np.newaxis,:], eta[:,np.newaxis]
+        ee = ee if self.cylinder_type == 'full' else (ee+1)/2
+        return ee * np.polyval(self.h, tt)
+
+    def vertical_polynomial(self, ell):
         if ell >= self.Lmax:
             raise ValueError(f'ell (={ell}) index exceeds maximum Lmax-1 (={self.Lmax-1})')
-        self._make_vertical_polynomials(eta)
-        if self.P is None:
-            raise ValueError('Never constructed the vertical polynomials.  Did you mean to call this with eta?')
-        return self.P[ell]
+        P = self.vertical_polynomials
+        if P is None:
+            raise ValueError('Basis constructed without eta argument')
+        return P[ell]
 
-    def radial_polynomial(self, ell, k, t=None):
+    def radial_polynomial(self, ell, k):
         if ell >= self.Lmax:
             raise ValueError(f'ell (={ell}) index exceeds maximum Lmax-1 (={self.Lmax-1})')
         if k >= _radial_size(self.Nmax, ell):
-            raise ValueError(f'k (={k}) index exceeds maximum Nmax-ell-1 (={_radial_size(self.Nmax, ell)})')
-        if self.Q is None:
-            raise ValueError('Never constructed the radial polynomials.  Did you mean to call this with t?')
-        return self.Q[ell][k]
+            raise ValueError(f'k (={k}) index exceeds maximum Nmax-ell-1 (={_radial_size(self.Nmax, ell)-1})')
+        Q = self.radial_polynomials
+        if Q is None:
+            raise ValueError('Basis constructed without t argument')
+        return Q[ell][k]
 
     def expand(self, coeffs):
         # Ensure we already constructed our basis functions
-        if self.P is None or self.Q is None:
+        P, Q = self.vertical_polynomials, self.radial_polynomials
+        if P is None or Q is None:
             raise ValueError('Never constructed the polynomial basis')
 
         # Check the coefficient size matches the basis
@@ -74,57 +144,30 @@ class Basis():
         # Iterate through each basis function, adding in its weighted contribution
         index = 0
         for ell in range(self.Lmax):
-            Pl = self.P[ell]
             for k in range(_radial_size(self.Nmax, ell)):
-                Qlk = self.Q[ell][k]
-                f += coeffs[index] * Pl[:,np.newaxis] * Qlk[np.newaxis,:]
+                f += coeffs[index] * P[ell][:,np.newaxis] * Q[ell][k][np.newaxis,:]
                 index += 1
         return f
 
-    def s(self, t=None):
-        if t is None:
-            if self.t is None:
-                raise ValueError('missing t argument')
-            t = self.t
-        return np.sqrt((1+t)/2)
-
-    def z(self, t=None, eta=None):
-        if t is None:
-            if self.t is None:
-                raise ValueError('missing t argument')
-            t = self.t
-        if eta is None:
-            if self.eta is None:
-                raise ValueError('missing eta argument')
-            eta = self.eta
-        tt, ee = t[np.newaxis,:], eta[:,np.newaxis]
-        ee = ee if self.cylinder_type == 'full' else (ee-1)/2
-        return ee * np.polyval(self.h, tt)
-
     def _make_vertical_polynomials(self, eta):
         if eta is not None:
-            self.eta = eta
-            self.P = jacobi.polynomials(self.Lmax, self.alpha, self.alpha, eta, dtype=self.dtype)
+            self.__eta = eta
+            self.__P = jacobi.polynomials(self.Lmax, self.alpha, self.alpha, eta, dtype=self.dtype)
 
     def _make_radial_polynomials(self, t):
         if t is not None:
-            self.t = t
+            self.__t = t
             if self.has_m_scaling:
-                prefactor = (1+t)**(self.m + self.sigma)
+                prefactor = (1+t)**((self.m + self.sigma)/2)
             else:
-                prefactor = (1+t)**self.sigma
+                prefactor = (1+t)**(self.sigma/2)
             if self.has_h_scaling:
                 ht = np.polyval(self.h, t)
             else:
                 ht = 1.
-            self.height = ht
-            poly = lambda ell: self.systems[ell].polynomials(_radial_size(self.Nmax, ell), t, dtype=self.dtype)
-            self.Q = [prefactor * ht**ell * poly(ell) for ell,system in enumerate(self.systems)]
-
-
-def _radial_jacobi_parameters(m, alpha, sigma, ell=None):
-    fn = lambda l: (alpha, m+sigma, (2*l+2*alpha+1,))
-    return fn(ell) if ell is not None else fn
+            systems = self.__systems
+            polys = lambda ell: systems[ell].polynomials(_radial_size(self.Nmax, ell), t, dtype=self.dtype)
+            self.__Q = [prefactor * ht**ell * polys(ell) for ell,system in enumerate(systems)]
 
 
 def _get_ell_modifiers(Lmax, alpha, dtype='float64', internal='float128'):
@@ -168,6 +211,11 @@ def coeff_sizes(Lmax, Nmax):
 
 def total_num_coeffs(Lmax, Nmax):
     return coeff_sizes(Lmax, Nmax)[1][-1]
+
+
+def _radial_jacobi_parameters(m, alpha, sigma, ell=None):
+    fn = lambda l: (alpha, m+sigma, (2*l+2*alpha+1,))
+    return fn(ell) if ell is not None else fn
 
 
 def _make_operator(dell, zop, sop, m, Lmax, Nmax, alpha, sigma, Lpad=0, Npad=0):
@@ -335,8 +383,9 @@ def vector_laplacian(cylinder_type, h, m, Lmax, Nmax, alpha, dtype='float64', in
 def normal_component(cylinder_type, h, m, Lmax, Nmax, alpha, surface, dtype='float64', internal='float128', exact=False):
     _check_cylinder_type(cylinder_type)
 
-    ops = aj_operators([h], dtype=internal, internal=internal)    
-    B, R, Id = ops('B'), ops('rhoprime'), ops('Id')
+    ops = aj_operators([h], dtype=internal, internal=internal)
+    B, Id = ops('B'), ops('Id')
+    R = ops('rhoprime', weighted=False)
     Zero = 0*Id
 
     if surface == 'z=h':
@@ -344,7 +393,9 @@ def normal_component(cylinder_type, h, m, Lmax, Nmax, alpha, surface, dtype='flo
         Lm = -2 * R @ B(+1)
         Lz = Id
         dn = 1 + R.codomain.dn
-    elif (cylinder_type, surface) == ('full', 'z=-h'):
+    elif surface == 'z=-h':
+        if cylinder_type != 'full':
+                raise ValueError('Half cylinder cannot be evaluated at z=-h')
         # If we're at the bottom flip the sign of the z component compared to the top
         N = normal_component('full', 'top', h, m, Lmax, Nmax, alpha, dtype=dtype, internal=internal, exact=exact).tocsr()
         n = total_num_coeffs(Lmax, Nmax)
@@ -365,9 +416,9 @@ def normal_component(cylinder_type, h, m, Lmax, Nmax, alpha, surface, dtype='flo
 
     zop = np.ones(Lmax)
     Npad = dn if exact else 0
-    make_op = lambda sigma, sop: _make_operator(0, zop, sop, m, Lmax, Nmax, alpha, sigma, Npad=Npad)
+    make_op = lambda sigma, sop: _make_operator(0, zop, sop, m, Lmax, Nmax, alpha, sigma, Npad=Npad).astype(dtype)
     ops = [make_op(sigma, L) for sigma, L in [(+1,Lp),(-1,Lm),(0,Lz)]]
-    return sparse.hstack([make_op(sigma, L) for sigma, L in [(+1,Lp),(-1,Lm),(0,Lz)]]).astype(dtype)
+    return sparse.hstack(ops)
 
 
 def boundary(cylinder_type, h, m, Lmax, Nmax, alpha, sigma, surface, dtype='float64', internal='float128'):
@@ -461,7 +512,8 @@ def project(cylinder_type, h, m, Lmax, Nmax, alpha, sigma, direction, shift=0, L
     elif direction == 's':
         # Size Lmax-halt, projecting onto all vertical coefficients of fixed total polynomial degree
         indices = offsets[1:]-1
-        col = sparse.hstack([C[:,indices[ell]-shift] for ell in range(Lmax-Lstop)])
+        Lend = Lmax+Lstop if Lstop <= 0 else Lstop
+        col = sparse.hstack([C[:,indices[ell]-shift] for ell in range(Lend)])
     else:
         raise ValueError(f'Invalid direction ({direction})')
     return col

paper/stretched_cylinder_inertial_waves.py
@@ -19,8 +19,113 @@ def scale_to_unity(f):
     if abs(np.min(f)) > np.max(f):
         f /= np.min(f)
     else:
-        f /= np.max(f)
+        maxf = np.max(f)
+        if maxf != 0:
+            f /= maxf
     return f
+
+
+def build_boundary(cylinder_type, h, m, Lmax, Nmax, alpha, exact=False):
+    operators = sc.operators(cylinder_type, h, m, Lmax, Nmax)
+    ncoeff = sc.total_num_coeffs(Lmax, Nmax)
+
+    if exact:
+        operators1 = sc.operators(cylinder_type, h, m, Lmax, Nmax+1)
+        Nmax1 = Nmax+1
+    else:
+        operators1 = operators
+        Nmax1 = Nmax
+
+    # Side Boundary Condition: e_{S} \cdot \vec{u} = 0 at s = 1
+    N = operators ('normal_component', alpha=alpha+1, surface='s=S', exact=exact)
+    B = operators1('boundary',         alpha=alpha+1, surface='s=S', sigma=0)
+    Z = sparse.lil_matrix((Lmax, ncoeff))
+    row1 = sparse.hstack([B @ N, Z])
+
+    # Top  Boundary Condition: \vec{n} \cdot \vec{u} = 0 at z = h(s)
+    N = operators ('normal_component', alpha=alpha+1, surface='z=h', exact=exact)
+    B = operators1('boundary',         alpha=alpha+1, surface='z=h', sigma=0)
+    Z = sparse.lil_matrix((Nmax1, ncoeff))
+    row2 = sparse.hstack([B @ N, Z])
+
+    # Bottom Boundary Condition:e_{Z} \cdot \vec{u} = 0 at z = 0
+    N = operators('normal_component', alpha=alpha+1, surface='z=0')
+    B = operators('boundary',         alpha=alpha+1, surface='z=0', sigma=0)
+    Z = sparse.lil_matrix((Nmax, ncoeff))
+    row3 = sparse.hstack([B @ N, Z])
+
+    # Combine the boundary condition equations into a single matrix
+    row = sparse.vstack([row1,row2,row3])
+    return {'s=S': row1, 'z=h': row2, 'z=0': row3, 'combined': row}
+
+
+def test_boundary():
+    omega = .001
+    h = [omega/(2+omega), 1.]
+
+    cylinder_type = 'full'
+    m, Lmax, Nmax, alpha = 30, 10, 30, 0
+
+    # Build the boundary operators
+    boundary = build_boundary(cylinder_type, h, m, Lmax, Nmax, alpha, exact=True)
+
+    evaluate_side = True
+    evaluate_top = True
+    evaluate_bottom = True
+
+    if evaluate_side:
+        surface = 's=S'
+        op = boundary[surface]
+        nullspace = sp.linalg.null_space(op.todense())
+
+        t, eta = np.array([1.]), np.linspace(-1,1,101)
+        bases = create_bases(cylinder_type, h, m, Lmax, Nmax, alpha, t, eta)
+
+        dim = np.shape(nullspace)[1]
+        errors = np.zeros(dim)
+        for i in range(dim):
+            evector = nullspace[:,i]
+            ncoeff = bases['p'].num_coeffs
+            fields, boundary_errors = expand_evector(evector, bases, names='all', return_boundary_errors=True, verbose=False)
+            errors[i] = boundary_errors[surface]
+        print(f'Max boundary surface {surface} error = ', max(errors))
+        assert max(errors) < 2e-11
+
+    if evaluate_top:
+        surface = 'z=h'
+        op = boundary[surface]
+        nullspace = sp.linalg.null_space(op.todense())
+
+        t, eta = np.linspace(-1,1,101), np.array([1.])
+        bases = create_bases(cylinder_type, h, m, Lmax, Nmax, alpha, t, eta)
+
+        dim = np.shape(nullspace)[1]
+        errors = np.zeros(dim)
+        for i in range(dim):
+            evector = nullspace[:,i]
+            ncoeff = bases['p'].num_coeffs
+            fields, boundary_errors = expand_evector(evector, bases, names='all', return_boundary_errors=True, verbose=False)
+            errors[i] = boundary_errors[surface]
+        print(f'Max boundary surface {surface} error = ', max(errors))
+        assert max(errors) < 3e-12
+
+    if evaluate_bottom:
+        surface = 'z=0'
+        op = boundary[surface]
+        nullspace = sp.linalg.null_space(op.todense())
+
+        t, eta = np.linspace(-1,1,101), np.array([0. if cylinder_type == 'full' else -1.])
+        bases = create_bases(cylinder_type, h, m, Lmax, Nmax, alpha, t, eta)
+
+        dim = np.shape(nullspace)[1]
+        errors = np.zeros(dim)
+        for i in range(dim):
+            evector = nullspace[:,i]
+            ncoeff = bases['p'].num_coeffs
+            fields, boundary_errors = expand_evector(evector, bases, names='all', return_boundary_errors=True, verbose=False)
+            errors[i] = boundary_errors[surface]
+        print(f'Max boundary surface {surface} error = ', max(errors))
+        assert max(errors) < 2e-13
 
 
 def build_matrices_tau(cylinder_type, h, m, Lmax, Nmax, alpha):
@@ -39,36 +144,33 @@ def build_matrices_tau(cylinder_type, h, m, Lmax, Nmax, alpha):
     L = sparse.bmat([[C, G], [D, Z]])
     M = -sparse.block_diag([I, I, I, Z])
 
-    # Side Boundary Condition: e_{S} \cdot \vec{u} = 0 at s = 1
-    N = operators('normal_component', alpha=alpha+1, surface='s=S')
-    B = operators('boundary',         alpha=alpha+1, surface='s=S', sigma=0)
-    Z = sparse.lil_matrix((Lmax, ncoeff))
-    row1 = sparse.hstack([B @ N, Z])
-
-    # Top  Boundary Condition: \vec{n} \cdot \vec{u} = 0 at z = h(s)
-    N = operators('normal_component', alpha=alpha+1, surface='z=h')
-    B = operators('boundary',         alpha=alpha+1, surface='z=h', sigma=0)
-    Z = sparse.lil_matrix((Nmax, ncoeff))
-    row2 = sparse.hstack([B @ N, Z])
-
-    # Bottom Boundary Condition: -e_{Z} \cdot \vec{u} = 0 at z = 0
-    N = operators('normal_component', alpha=alpha+1, surface='z=0')
-    B = operators('boundary',         alpha=alpha+1, surface='z=0', sigma=0)
-    Z = sparse.lil_matrix((Nmax, ncoeff))
-    row3 = sparse.hstack([B @ N, Z])
-
-    # Combine the boundary condition equations into a single matrix
-    row = sparse.vstack([row1,row2,row3])
+    # Build the combined boundary condition
+    row = build_boundary(cylinder_type, h, m, Lmax, Nmax, alpha)['combined']
 
     # Tau projections for enforcing the boundaries
-    col1 = operators('project', alpha=alpha+0, sigma=+1, direction='z')
-    col2 = operators('project', alpha=alpha+0, sigma=+1, direction='s', Lstop=1)
-    col3 = operators('project', alpha=alpha+0, sigma=-1, direction='s')
-    col4 = operators('project', alpha=alpha+0, sigma=0,  direction='z')
-    col5 = operators('project', alpha=alpha+0, sigma=0,  direction='s', Lstop=1)
-    colp = sparse.hstack([col1,col2])
-    colm = sparse.hstack([col3])
+    """
+    # Pretty good!
+    col1 = operators('project', alpha=alpha, sigma=+1, direction='s')
+    col2 = operators('project', alpha=alpha, sigma=-1, direction='z')
+    col3 = operators('project', alpha=alpha, sigma=-1, direction='s', Lstop=-1)
+    col4 = operators('project', alpha=alpha, sigma=0,  direction='z')
+    col5 = operators('project', alpha=alpha, sigma=0,  direction='s', Lstop=-1)
+    colp = sparse.hstack([col1])
+    colm = sparse.hstack([col2,col3])
     colz = sparse.hstack([col4,col5])
+    """
+    # Quite good!
+#    col1 = operators('project', alpha=alpha, sigma=+1, direction='s', Lstop=-1)
+#    col2 = operators('project', alpha=alpha, sigma=-1, direction='s')
+    col1 = operators('project', alpha=alpha, sigma=+1, direction='s')
+    col2 = operators('project', alpha=alpha, sigma=-1, direction='s', Lstop=-1)
+    col3 = operators('project', alpha=alpha, sigma=0,  direction='z')
+    col4 = operators('project', alpha=alpha, sigma=0,  direction='z', shift=1)
+    col5 = operators('project', alpha=alpha, sigma=0,  direction='s', Lstop=-2)
+    colp = sparse.hstack([col1])
+    colm = sparse.hstack([col2])
+    colz = sparse.hstack([col3,col4,col5])
+
     cols = [colp, colm, colz]
     col = sparse.vstack([sparse.block_diag(cols), 0*sparse.hstack(cols)])
 
@@ -111,16 +213,12 @@ def solve_eigenproblem(omega, cylinder_type, h, m, Lmax, Nmax, boundary_method, 
     return data
 
 
-def expand_evector(evector, Lmax, Nmax, bases, boundary_method, names='all'): 
+def expand_evector(evector, bases, names='all', return_boundary_errors=False, verbose=True): 
     evector = evector.astype(np.complex128)
 
-    ncoeffs = sc.total_num_coeffs(Lmax, Nmax)
+    ncoeffs = bases['p'].num_coeffs
     evector[:3*ncoeffs] /= 1j
-
-    Up = evector[0*ncoeffs:1*ncoeffs]
-    Um = evector[1*ncoeffs:2*ncoeffs]
-    W  = evector[2*ncoeffs:3*ncoeffs]
-    P  = evector[3*ncoeffs:4*ncoeffs]
+    Up, Um, W, P = [evector[i*ncoeffs:(i+1)*ncoeffs] for i in range(4)]
 
     larger = lambda f: f.real if np.max(abs(f.real)) >= np.max(abs(f.imag)) else f.imag
 
@@ -131,29 +229,51 @@ def expand_evector(evector, Lmax, Nmax, bases, boundary_method, names='all'):
         um = bases['um'].expand(Um)
         u, v = 1/np.sqrt(2)*(up + um), -1j/np.sqrt(2)*(up - um)
         u, v = larger(u), larger(v)
-        side = np.max(abs(u[:,-1]))
     if names == 'all' or 'w' in names:
         w = bases['w'].expand(W)
         w = larger(w)
-        top, bot = np.max(abs(w[0,:])), np.max(abs(w[-1,:]))
     if names == 'all' or 'p' in names:
         p = bases['p'].expand(P)
-        p = larger(p)
-        p = scale_to_unity(p)
+        p = scale_to_unity(larger(p))
 
-    if not np.all(np.isnan([top,bot,side])):
-        print('Boundary errors: ', top, bot, side)
+    basis = bases['p']
+    h, t, eta = basis.h, basis.t, basis.eta
+
+    # Check the boundary
+    if u is not None:
+        index = np.argmin(abs(t-1))
+        side = np.max(abs(u[:,index]))
+    if w is not None:
+        z0 = bases['w'].z()[:,0]
+        index = np.argmin(abs(z0))
+        bot = np.max(abs(w[index,:]))
+    if u is not None and w is not None:
+        basis = bases['p']
+        index = np.argmin(abs(eta-1))
+        utop, wtop = u[index,:], w[index,:]
+        hprime = np.polyval(np.polyder(h), t)
+        ndotu = -2*np.sqrt(2*(1+t)) * hprime * utop + wtop
+        top = np.max(abs(ndotu))
+
+    if verbose and not np.all(np.isnan([top,bot,side])):
+        print(f'Boundary errors: top = {top}, bottom = {bot}, side = {side}')
 
     fields = {'u': u, 'v': v, 'w': w, 'p': p}
+    if return_boundary_errors:
+        return fields, {'z=h': top, 'z=0': bot, 's=S': side}
     return fields
 
 
-def plot_spectrum_callback(index, evalues, evectors, m, Lmax, Nmax, s, eta, bases, boundary_method, fig=None, ax=None):
-    fields = expand_evector(evectors[:,index], Lmax, Nmax, bases, boundary_method, names=['p'])
+def plot_spectrum_callback(index, evalues, evectors, m, Lmax, Nmax, bases, boundary_method, fig=None, ax=None):
+    fields = expand_evector(evectors[:,index], bases, names='all')
+
+    fieldname = 'p'
+    basis = bases[fieldname]
+    s, z = basis.s(), basis.z()
 
     if fig is None or ax is None:
         fig, ax = plt.subplots()
-    im = ax.pcolormesh(s, eta, fields['p'])
+    im = ax.pcolormesh(s, z, fields[fieldname])
     fig.show()
     return fig, ax
 
@@ -169,25 +289,28 @@ def compare_mode(evalues, evectors, n, k, evalue_targets, roots, bases, boundary
     print(f'Mode (m,n,k) = ({m},{n},{k}), λ = {evalue_target}')
     print('    Eigenvalue error: ', abs(evalue_target - evalues[index]))
 
-    fields = expand_evector(evectors[:,index], Lmax, Nmax, bases, boundary_method, names=[field])
+    fields = expand_evector(evectors[:,index], bases, names='all')
     p = fields['p']
 
     # Compute the analytic_mode and make sure we have the right sign
-    s, eta = basis.s()[np.newaxis,:], basis.eta[:,np.newaxis]
-    zcyl = (eta+1)/2 * basis.height[np.newaxis,:]
-    zcart = np.linspace(0,1,len(eta))
+    s, zcyl = basis.s(), basis.z()
+    zcart = np.linspace(0,1,np.shape(zcyl)[0])
     f = analytic_mode(m, n, k, roots=roots, radius=1.)(s,zcart)
-    if np.max(abs(p+f)) < np.max(abs(p-f)):
-        f = -f
 
     if plot:
         fig, ax = plt.subplots(1,2,figsize=plt.figaspect(1/2))
-        ax[0].pcolormesh(s, zcyl, p)
-        ax[1].pcolormesh(s, zcart, f, shading='gouraud')
+        ax[0].pcolormesh(s, zcyl, p, shading='gouraud')
+        ax[1].pcolormesh(s, zcart, f)
         ax[0].set_title(f'Computed Mode, (m,n,k) = ({m},{n},{k})')
         ax[1].set_title(f'Analytic Mode, (m,n,k) = ({m},{n},{k})')
         for a in ax:
             a.set_aspect('equal')
+
+
+def create_bases(cylinder_type, h, m, Lmax, Nmax, alpha, t, eta):
+    pbasis =  sc.Basis(cylinder_type, h, m, Lmax, Nmax, alpha=alpha+0, sigma=0,   t=t, eta=eta)
+    vbases = [sc.Basis(cylinder_type, h, m, Lmax, Nmax, alpha=alpha+1, sigma=sig, t=t, eta=eta) for sig in [+1,-1,0]]
+    return {'p': pbasis, 'up': vbases[0], 'um': vbases[1], 'w': vbases[2]}
 
 
 def plot_solution(data):
@@ -197,12 +320,9 @@ def plot_solution(data):
     boundary_method = data['boundary_method']
     alpha = data.pop('alpha', 0)
 
-    s = np.linspace(0,1,400)
-    t = 2*s**2-1
+    t = np.linspace(-1,1,400)
     eta = np.linspace(-1,1.,301)
-    pbasis =  sc.Basis(cylinder_type, h, m, Lmax, Nmax, alpha=alpha+0, sigma=0,   t=t, eta=eta)
-    vbases = [sc.Basis(cylinder_type, h, m, Lmax, Nmax, alpha=alpha+1, sigma=sig, t=t, eta=eta) for sig in [+1,-1,0]]
-    bases = {'p': pbasis, 'up': vbases[0], 'um': vbases[1], 'w': vbases[2]}
+    bases = create_bases(cylinder_type, h, m, Lmax, Nmax, alpha, t, eta)
 
     n, kmax = 3, 7
     evalue_targets, roots = analytic_eigenvalues(m, n, kmax+1, maxiter=20, radius=1.)
@@ -210,10 +330,9 @@ def plot_solution(data):
         plot = k == kmax//2
         compare_mode(evalues, evectors, n, k, evalue_targets, roots, bases, boundary_method, plot=plot)
 
-    z = (eta[:,np.newaxis]+1)/2 * np.polyval(h, t[np.newaxis,:])
 
     def onpick(index):
-        return plot_spectrum_callback(index, evalues, evectors, m, Lmax, Nmax, s, z, bases, boundary_method)
+        return plot_spectrum_callback(index, evalues, evectors, m, Lmax, Nmax, bases, boundary_method)
 
     fig, ax = plot_spectrum(evalues, onpick=onpick)
     ax.set_xlim([-2.1,2.1])
@@ -237,4 +356,5 @@ def main():
 
 if __name__=='__main__':
     main()
+#    test_boundary()
 

tests/test_stretched_cylinder.py
@@ -4,12 +4,22 @@ from scipy import sparse
 import matplotlib.pyplot as plt
 from gyropoly import stretched_cylinder as sc
 
+np.random.seed(37)
+
 Omega = 1.
 h = [Omega/(2+Omega), 1.]
-m, Lmax, Nmax = 10, 4, 10
+m, Lmax, Nmax = 3, 4, 10
 alpha = 1.
 cylinder_type = 'full'
 operators = sc.operators(cylinder_type, h, m=m, Lmax=Lmax, Nmax=Nmax, alpha=alpha)
+
+
+def create_scalar_basis(Lmax, Nmax, alpha, t, eta):
+    return sc.Basis(cylinder_type, h, m, Lmax, Nmax, alpha=alpha, sigma=0, eta=eta, t=t)
+
+
+def create_vector_bases(Lmax, Nmax, alpha, t, eta):
+    return {key: sc.Basis(cylinder_type, h, m, Lmax, Nmax, alpha=alpha, sigma=s, eta=eta, t=t) for key, s in [('up', +1), ('um', -1), ('w', 0)]}
 
 
 def test_gradient():
@@ -24,10 +34,102 @@ def test_laplacian():
     ax.spy(op)
 
 
+def test_ndot_top():
+    # Build the operator
+    exact = True
+    dn = 1 if exact else 0
+    op = operators('normal_component', surface='z=h', exact=exact)
+
+    # Construct the coefficient vector and apply the operator
+    ncoeff = sc.total_num_coeffs(Lmax, Nmax)
+    c = 2*np.random.rand(3*ncoeff) - 1
+    d = op @ c
+
+    # Construct the bases
+    t = np.linspace(-1,1,100)
+    eta = np.linspace(-1,1,101)
+    scalar_basis = create_scalar_basis(Lmax, Nmax+dn, alpha, t, eta)
+    vector_basis = create_vector_bases(Lmax, Nmax,    alpha, t, eta)
+    s, z = scalar_basis.s(), scalar_basis.z()
+
+    # Evaluate the operator output in the scalar basis
+    ndotu = scalar_basis.expand(d)
+
+    # Compute the normal component in grid space
+    Cp, Cm, Cz = [c[i*ncoeff:(i+1)*ncoeff] for i in range(3)]
+    up, um, w = [vector_basis[key].expand(coeffs) for key,coeffs in [('up', Cp), ('um', Cm), ('w', Cz)]]
+    u = 1/np.sqrt(2) * (up + um)
+    hp = np.polyval(np.polyder(scalar_basis.h), t)
+    ndotu_grid = -2*np.sqrt(2) * hp * np.sqrt(1+t) * u + w
+
+    # Compute the error
+    error = ndotu - ndotu_grid
+    assert np.max(abs(error)) < 5e-14
+
+
+def test_ndot_bottom():
+    op = operators('normal_component', surface='z=0')
+
+    # Construct the coefficient vector and apply the operator
+    ncoeff = sc.total_num_coeffs(Lmax, Nmax)
+    c = 2*np.random.rand(3*ncoeff) - 1
+    d = op @ c
+
+    # Construct the bases
+    t = np.linspace(-1,1,100)
+    eta = np.linspace(-1,1,101)
+    scalar_basis = create_scalar_basis(Lmax, Nmax, alpha, t, eta)
+    vector_basis = create_vector_bases(Lmax, Nmax, alpha, t, eta)
+    s, z = scalar_basis.s(), scalar_basis.z()
+
+    # Evaluate the operator output in the scalar basis
+    ndotu = scalar_basis.expand(d)
+
+    # Compute the normal component in grid space
+    Cz = c[2*ncoeff:3*ncoeff]
+    w = vector_basis['w'].expand(Cz)
+    ndotu_grid = -w
+
+    # Compute the error
+    error = ndotu - ndotu_grid
+    assert np.max(abs(error)) < 1e-16
+
+
+def test_ndot_side():
+    # Build the operator
+    exact = True
+    dn = 1 if exact else 0
+    op = operators('normal_component', surface='s=S', exact=exact)
+
+    # Construct the coefficient vector and apply the operator
+    ncoeff = sc.total_num_coeffs(Lmax, Nmax)
+    c = 2*np.random.rand(3*ncoeff) - 1
+    d = op @ c
+
+    # Construct the bases
+    t = np.linspace(-1,1,100)
+    eta = np.linspace(-1,1,101)
+    scalar_basis = create_scalar_basis(Lmax, Nmax+dn, alpha, t, eta)
+    vector_basis = create_vector_bases(Lmax, Nmax,    alpha, t, eta)
+    s, z = scalar_basis.s(), scalar_basis.z()
+
+    # Evaluate the operator output in the scalar basis
+    ndotu = scalar_basis.expand(d)
+
+    # Compute the normal component in grid space
+    Cp, Cm, Cz = [c[i*ncoeff:(i+1)*ncoeff] for i in range(3)]
+    up, um, w = [vector_basis[key].expand(coeffs) for key,coeffs in [('up', Cp), ('um', Cm), ('w', Cz)]]
+    ndotu_grid = 1/np.sqrt(2) * s * (up + um)
+
+    # Compute the error
+    error = ndotu - ndotu_grid
+    assert np.max(abs(error)) < 1e-13
+
+
 def test_normal_component():
-    op = operators('normal_component', surface='z=h')
-    fig, ax = plt.subplots()
-    ax.spy(op)
+    test_ndot_top()
+    test_ndot_bottom()
+    test_ndot_side()
 
 
 def rank_deficiency(mat):
@@ -105,7 +207,7 @@ def test_project():
     top_shifts = [1,0]     # size Nmax-(Lmax-2) and Nmax-(Lmax-1), total = 2*Nmax-2*Lmax+3
     side_shifts = [2,1,0]  # total size 3*(Lmax-2) = 3*Lmax-6, top+side = 2*Nmax+Lmax-3 ok
     opt = [project(direction='z', shift=shift) for shift in top_shifts]
-    ops = [project(direction='s', shift=shift, Lstop=2) for shift in side_shifts]
+    ops = [project(direction='s', shift=shift, Lstop=-2) for shift in side_shifts]
 
     all_ops = ops + opt
     ns = [np.shape(op)[1] for op in all_ops]
@@ -117,8 +219,8 @@ def test_project():
     plt.spy(op)
 
 if __name__=='__main__':
-    test_gradient()
-    test_laplacian()
+#    test_gradient()
+#    test_laplacian()
     test_normal_component()
     test_boundary()
     test_project()