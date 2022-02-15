ba7e7cff18f14c2ddd30d7f7d7fd914eaf1489ec
Abe Ellison <abe.ellison@gmail.com>
2022-02-15 08:41:53 -0700

    Add profile_method decorator

--- gyropoly/decorators.py
+++ gyropoly/decorators.py
@@ -1,17 +1,29 @@
 import time
 from functools import lru_cache
 
-__all__ = ['profile', 'cached']
+__all__ = ['profile', 'profile_method', 'cached']
+
+def _profile(name, fun, *args, **kwargs):
+    print(f'Running {name}...')
+    t1 = time.time()
+    result = fun(*args, **kwargs)
+    t2 = time.time()
+    duration = t2 - t1
+    print(f'  {name} executed in {duration:.4f}s')
+    return result
+
 
 def profile(fun):
     name = repr(fun.__name__)
     def wrap(*args, **kwargs):
-        print(f'Running {name}...')
-        t1 = time.time()
-        result = fun(*args, **kwargs) 
-        t2 = time.time()
-        print(f'  {name} executed in {(t2-t1):.4f}s')
-        return result
+        return _profile(name, fun, *args, **kwargs)
+    return wrap
+
+
+def profile_method(fun):
+    def wrap(self, *args, **kwargs):
+        name = str(type(self)) + f'.{fun.__name__}'
+        return _profile(name, fun, self, *args, **kwargs)
     return wrap
 
 
@@ -32,14 +44,14 @@ def deep_list_to_tuple(a):
         raise TypeError(f'Cannot convert value to a hashable type (value = {a})')
 
 
-def convert_list_arguments_to_tuples(func):
+def convert_list_arguments_to_tuples(fun):
     def wrapper(*args, **kwargs):
         args = list(args)
         for i in range(len(args)):
             args[i] = deep_list_to_tuple(args[i])
         for key,value in kwargs.items():
             kwargs[key] = deep_list_to_tuple(value)
-        return func(*args, **kwargs)
+        return fun(*args, **kwargs)
     return wrapper
 
 
