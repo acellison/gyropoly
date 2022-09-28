849f529ea48487ef738f581c3d416315d5fb3a29
Abe Ellison <abe.ellison@gmail.com>
2022-09-28 11:18:20 -0600

    Add stretched annulus damped inertial waves solver

---
+++ paper/stretched_annulus_hydrodynamics.py
@@ -0,0 +1,280 @@
+import os, pickle
+import numpy as np
+import scipy as sp
+from scipy import sparse
+
+import matplotlib
+matplotlib.rcParams.update({'font.size': 14})
+import matplotlib.pyplot as plt
+
+from spherinder.eigtools import eigsort, plot_spectrum, scipy_sparse_eigs
+
+from gyropoly import config
+config.parallel = True
+
+import gyropoly.stretched_annulus as domain
+from gyropoly.decorators import cached, profile
+
+
+g_file_prefix = 'stretched_annulus_hydrodynamics'
+g_recurrence_kwargs = {'use_jacobi_quadrature': False}
+
+
+def Lshift(sigma):
+    """Vertical velocity has one fewer vertical degree than the other components in the Galerkin formulation"""
+    return 1 if sigma == 0 else 0
+
+
+def combined_projection(geometry, m, Lmax, Nmax, alpha, sigma):
+    def make_op(direction, shift, Lstop=0): 
+        return domain.project(geometry, m, Lmax, Nmax, alpha, sigma=sigma, direction=direction, shift=shift, Lstop=Lstop)
+
+    top_shifts = [1,0]     # size Nmax-(Lmax-2) and Nmax-(Lmax-1), total = 2*Nmax-2*Lmax+3
+    if geometry.degree == 0:
+        side_shifts = [0]  # total size 3*(Lmax-2) = 3*Lmax-6, top+side = 2*Nmax+Lmax-3 ok
+    else:
+        side_shifts = [3,2,1,0]  # total size 4*(Lmax-2) = 4*Lmax-8, top+side = 2*Nmax+2*Lmax-5 ok
+    opt = [make_op(direction='z', shift=shift) for shift in top_shifts]
+    ops = [make_op(direction='s', shift=shift, Lstop=-2) for shift in side_shifts]
+    return sparse.hstack(opt+ops)
+
+
+def build_projections(geometry, m, Lmax, Nmax, alpha, boundary_method):
+    dN = 2+(2-int(geometry.root_h))*geometry.degree
+    make_op = lambda sigma, dalpha, dL: combined_projection(geometry, m, Lmax+2-dL, Nmax+dN, alpha+dalpha, sigma)
+    return sparse.block_diag([make_op(sigma, dalpha, dL) for sigma, dalpha, dL in [(+1,1,0), (-1,1,0), (0,1,Lshift(0)), (0,0,0)]])
+
+
+@cached
+def galerkin_matrix(geometry, m, Lmax, Nmax, alpha):
+    Sp, Sm, Sz = [domain.convert(geometry, m, Lmax-Lshift(sigma), Nmax, alpha+1, sigma=sigma, adjoint=True, recurrence_kwargs=g_recurrence_kwargs) for sigma in [+1,-1,0]]
+    I = sparse.eye(domain.total_num_coeffs(geometry, Lmax, Nmax))
+    return sparse.block_diag([Sp,Sm,Sz,I])
+
+
+@profile
+def build_matrices_galerkin(geometry, m, Lmax, Nmax, Ekman, alpha):
+    dL, dN = 2, 2+(2-int(geometry.root_h))*geometry.degree
+    operatorsu = domain.operators(geometry, m, Lmax+dL, Nmax+dN, recurrence_kwargs=g_recurrence_kwargs)
+    operatorsp = domain.operators(geometry, m, Lmax,    Nmax,    recurrence_kwargs=g_recurrence_kwargs)
+
+    ncoeffu = domain.total_num_coeffs(geometry, Lmax+dL, Nmax+dN)
+    ncoeffp = domain.total_num_coeffs(geometry, Lmax,    Nmax)
+    Z = sparse.lil_matrix((ncoeffu,ncoeffp))
+
+    # Bulk Equations
+    G = operatorsp('gradient',   alpha=alpha+1)      # alpha+1 -> alpha+2
+    D = operatorsu('divergence', alpha=alpha)        # alpha   -> alpha+1
+    Lap = operatorsu('vector_laplacian', alpha=alpha)  # alpha   -> alpha+2
+    Ap, Am, Az = [operatorsu('convert', alpha=alpha, sigma=sigma, ntimes=2) for sigma in [+1,-1,0]]
+
+    # Resize the gradient into the Lmax+dL, Nmax+dN shape
+    Gs = [G[i*ncoeffp:(i+1)*ncoeffp,:] for i in range(3)]
+    G = sparse.vstack([domain.resize(geometry, mat, Lmax, Nmax, Lmax+dL, Nmax+dN) for sigma,mat in zip([+1,-1,0], Gs)])
+
+    # Resize the vertical velocity down to Lmax-1
+    lengths, offsets = domain.coeff_sizes(geometry, Lmax+dL, Nmax+dN)
+    G = G.tocsr()[:-lengths[-1],:]
+    D = D.tocsr()[:,:-lengths[-1]]
+    Lap = Lap.tocsr()[:-lengths[-1],:-lengths[-1]]
+    Az = Az.tocsr()[:-lengths[-1],:-lengths[-1]]
+
+    Cor = sparse.block_diag([2j*Ap, -2j*Am, 0*Az])
+    C = Cor - Ekman * Lap
+
+    # Construct the bulk system
+    L = sparse.bmat([[C, G], [D, Z]])
+    M = -sparse.block_diag([Ap, Am, Az, Z])
+
+    # Galerkin recombine the system for no slip boundaries
+    S = galerkin_matrix(geometry, m, Lmax, Nmax, alpha)
+    L, M = L @ S, M @ S
+
+    # Tau projections for enforcing the boundaries
+    col = build_projections(geometry, m, Lmax, Nmax, alpha, boundary_method='galerkin')
+    L = sparse.hstack([L,   col])
+    M = sparse.hstack([M, 0*col])
+
+    return L, M
+
+
+def _get_directory(prefix='data'):
+    directory = os.path.join(prefix, g_file_prefix)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return directory
+
+  
+def solve_eigenproblem(geometry, m, Lmax, Nmax, boundary_method, omega, Ekman, alpha=0, force_construct=True, force_solve=True, nev='all', evalue_target=None):
+    # Construct the data filename
+    alphastr = '' if alpha == 0 else f'-alpha={alpha}'
+    tarstr = f'-evalue_target={evalue_target}' if nev != 'all' else ''
+
+    directory = _get_directory('data')
+    prefix = os.path.join(directory, f'{g_file_prefix}-{geometry}-omega={float(omega)}-m={m}-Ekman={Ekman}-Lmax={Lmax}-Nmax={Nmax}{alphastr}-{boundary_method}')
+    matrix_filename = prefix + '-matrices.pckl'
+    esolve_filename = prefix + f'-esolve-nev={nev}{tarstr}.pckl'
+
+    base_data = {'geometry': geometry, 'boundary_method': boundary_method,
+                 'omega': omega, 'm': m, 'Lmax': Lmax, 'Nmax': Nmax, 'alpha': alpha, 'Ekman': Ekman}
+
+    if force_solve or not os.path.exists(esolve_filename):
+        # Build or load the matrices
+        if force_construct or not os.path.exists(matrix_filename):
+            print('  Building matrices...')
+            build_matrices = build_matrices_galerkin if boundary_method == 'galerkin' else build_matrices_tau
+            L, M = build_matrices(geometry, m, Lmax, Nmax, Ekman, alpha=alpha)
+            if boundary_method == 'galerkin':
+                S = galerkin_matrix(geometry, m, Lmax, Nmax, alpha)
+            else:
+                S = None
+            matrix_data = base_data.copy()
+            matrix_data['L'] = L
+            matrix_data['M'] = M
+            matrix_data['S'] = S
+            with open(matrix_filename, 'wb') as file:
+                pickle.dump(matrix_data, file)
+        else:
+            print('  Loading matrices...')
+            with open(matrix_filename, 'rb') as file:
+                matrix_data = pickle.load(file)
+            L, M, S = [matrix_data[key] for key in ['L', 'M', 'S']]
+
+        print('Eigenproblem size: ', np.shape(L))
+
+        # Solve the eigenproblem
+        if nev == 'all':
+            evalues, evectors = eigsort(L.todense(), M.todense(), profile=True)
+        else:
+            matsolver = 'SuperluColamdFactorized'
+#            matsolver = 'UmfpackFactorized64'
+            evalues, evectors = scipy_sparse_eigs(L, M, N=nev, target=evalue_target, matsolver=matsolver, profile=True)
+
+        # Recombine the eigenvectors
+        if boundary_method == 'galerkin':
+            I = sparse.eye(np.shape(evectors)[0]-np.shape(S)[1])
+            S = sparse.block_diag([S,I])
+            evectors = S @ evectors
+
+        # Save the result
+        esolve_data = base_data.copy()
+        esolve_data['evalues'] = evalues
+        esolve_data['evectors'] = evectors
+        with open(esolve_filename, 'wb') as file:
+            pickle.dump(esolve_data, file)
+
+    else:
+        with open(esolve_filename, 'rb') as file:
+            esolve_data = pickle.load(file)
+    return esolve_data
+
+
+def expand_evector(evector, bases, names='all', verbose=True): 
+    lengths = [bases[key].num_coeffs for key in ['up', 'um', 'w', 'p']]
+    offsets = np.append(0, np.cumsum(lengths))
+
+    Up, Um, W, P = [evector[offsets[i]:offsets[i+1]] for i in range(4)]
+
+    larger = lambda f: f.real if np.max(abs(f.real)) >= np.max(abs(f.imag)) else f.imag
+
+    u, v, w, p = (None,)*4
+    if names == 'all' or 'u' in names or 'v' in names:
+        up = bases['up'].expand(Up)
+        um = bases['um'].expand(Um)
+        u, v = 1/np.sqrt(2)*(up + um), -1j/np.sqrt(2)*(up - um)
+        u, v = larger(u), larger(v)
+    if names == 'all' or 'w' in names:
+        w = bases['w'].expand(W)
+        w = larger(w)
+    if names == 'all' or 'p' in names:
+        p = bases['p'].expand(P)
+        p = larger(p)
+
+    def check_boundary(field, name):
+        if field is not None:
+            top, bottom, side = [np.max(abs(f)) for f in [field[-1,:], field[0,:], field[:,-1]]]
+            print(f'  Boundary error for {name}: top: {top}, bottom: {bottom}, side: {side}')
+    check_boundary(u, 'u')
+    check_boundary(v, 'v')
+    check_boundary(w, 'w')
+
+    fields = {'u': u, 'v': v, 'w': w, 'p': p}
+    return fields
+
+
+def plot_spectrum_callback(index, evalues, evectors, bases, fig=None, ax=None):
+    fields = expand_evector(evectors[:,index], bases, names=['p'])
+
+    fieldname = 'p'
+    basis = bases[fieldname]
+    s, z = basis.s(), basis.z()
+
+    if fig is None or ax is None:
+        scale = 1 if basis.geometry.cylinder_type == 'half' else 2
+        zmax, smax = np.max(z), np.max(s)
+        fig, ax = plt.subplots(figsize=plt.figaspect(scale*zmax/smax))
+    domain.plotfield(s, z, fields[fieldname], fig, ax, colorbar=False)
+    fig.set_tight_layout(True)
+    fig.show()
+    return fig, ax
+
+
+def create_bases(geometry, m, Lmax, Nmax, alpha, t, eta, boundary_method):
+    if boundary_method == 'galerkin':
+        dL, dN = 2, 2+(2-int(geometry.root_h))*geometry.degree
+    else:
+        dL, dN = 0, 0
+    vbases = [domain.Basis(geometry, m, Lmax+dL-Lshift(sig), Nmax+dN, alpha=alpha,   sigma=sig, t=t, eta=eta, recurrence_kwargs=g_recurrence_kwargs) for sig in [+1,-1,0]]
+    pbasis =  domain.Basis(geometry, m, Lmax,                Nmax,    alpha=alpha+1, sigma=0,   t=t, eta=eta, recurrence_kwargs=g_recurrence_kwargs)
+    return {'p': pbasis, 'up': vbases[0], 'um': vbases[1], 'w': vbases[2]}
+
+
+def plot_solution(data):
+    boundary_method = data['boundary_method']
+    geometry, m, Lmax, Nmax, alpha = [data[key] for key in ['geometry', 'm', 'Lmax', 'Nmax', 'alpha']]
+    evalues, evectors = [data[key] for key in ['evalues', 'evectors']]
+
+    t = np.linspace(-1,1,400)
+    eta = np.linspace(-1.,1.,301)
+    bases = create_bases(geometry, m, Lmax, Nmax, alpha, t, eta, boundary_method)
+
+    def onpick(index):
+        return plot_spectrum_callback(index, evalues, evectors, bases)
+
+    fig, ax = plot_spectrum(evalues, onpick=onpick)
+    fig.set_tight_layout(True)
+
+
+def main():
+    cylinder_type, m, Lmax, Nmax, Ekman, alpha, omega, radii, nev = 'full', 14, 40, 160, 1e-5, 0, 2, (0.5, 1.), 100
+
+    boundary_method = 'galerkin'
+    force_construct, force_solve = (True,True)
+#    force_construct, force_solve = (False,False)
+    plot_height = False
+
+    evalue_target = 0.
+
+    H = 0.5 if cylinder_type == 'full' else 1.
+    hs = 2*H*np.array([omega/(2+omega), 1/(2+omega)])
+    ht = domain.scoeff_to_tcoeff(radii, hs)
+
+    geometry = domain.Geometry(cylinder_type=cylinder_type, hcoeff=ht, radii=radii)
+
+    if plot_height:
+        geometry.plot_height()
+        plt.show()
+
+    print(f'geometry: {geometry}, m = {m}, Lmax = {Lmax}, Nmax = {Nmax}, alpha = {alpha}, omega = {omega}')
+    data = solve_eigenproblem(geometry, m, Lmax, Nmax, boundary_method, omega, \
+                              Ekman=Ekman, alpha=alpha, \
+                              force_construct=force_construct, force_solve=force_solve, \
+                              nev=nev, evalue_target=evalue_target)
+    plot_solution(data)
+
+
+if __name__=='__main__':
+#    test_boundary()
+    main()
+    plt.show()
+
