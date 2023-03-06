6c584d13fc769e47c3c1329d6c6edf1f0383772e
Abe Ellison <abe.ellison@gmail.com>
2023-03-06 08:14:05 -0700

    Update dependencies

--- setup.py
+++ setup.py
@@ -4,4 +4,14 @@ from setuptools import setup
 setup(
     packages=['gyropoly'],
     version='0.1'
+    install_requires=[
+        'numpy',
+        'scipy',
+        'matplotlib',
+        'sympy',
+        'mpmath',
+        'pathos',
+        'dedalus_sphere',
+        'scikit-umfpack',
+    ]
 )
